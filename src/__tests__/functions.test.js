d6da52c64af65a32f0b13e02b91f04ffb6d044ee
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-11 11:03:56 -0700

    initial save

---
+++ src/__tests__/functions.test.js
@@ -0,0 +1,38 @@
+import {sum} from '../functions'
+// const {sum} = require('../functions')
+
+// most basic test
+test('2 equals 2', () => {
+    expect(2).toBe(2)
+})
+
+test('sum of 1 and 2 should be 3', ()=> {
+    expect(sum(1,2)).toBe(3)
+})
+
+test('works with strings', ()=> {
+    expect(sum('1', '5')).toBe(6)
+})
+
+test('object has id', () => {
+    expect({id: 4}).toEqual({id: 4})
+})
+
+//not keyword
+test('2 does not equal 4', ()=> {
+    expect(2).not.toBe(4)
+})
+
+test('true equals true', ()=> {
+    expect(true).toBeTruthy()
+})
+
+test('falsy values are falsy', ()=> {
+    expect(null).toBeFalsy()
+    expect(0).toBeFalsy()
+    expect(false).toBeFalsy()
+    expect('').toBeFalsy()
+    expect(NaN).toBeFalsy()
+    expect(undefined).toBeFalsy()
+})
+
